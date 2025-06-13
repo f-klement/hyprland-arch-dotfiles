c7543424c2870efbcac9c2da06c3fca78f8a992d
Florian Klement <florian.klement@gmx.net>
2025-06-13 19:32:39 +0200

    fix wallpaper

--- .config/hypr/initial-boot.sh
+++ .config/hypr/initial-boot.sh
@@ -1,4 +1,4 @@
-        #!/bin/bash
+#!/bin/bash
 # A bash script designed to run only once dotfiles installed
 
 # THIS SCRIPT CAN BE DELETED ONCE SUCCESSFULLY BOOTED!! And also, edit ~/.config/hypr/configs/Settings.conf
